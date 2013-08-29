3bdc5fd47e2b34c5a2f7be409ed6d9e36079e4ab
Alex Laughnan <laughnan@pdx.edu>
2013-08-29 12:35:14 -0700

    removed unnecessary files for testing/development practice.

--- Tests/Loc/bmlet-login.js
+++
@@ -1,1 +0,0 @@
-popupWindow = window.open('http://guimelsa.com/celly/html-forms/login-form.html','popUpWindow','height=400,width=300,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes');
--- Tests/Travis/script6.js
+++
@@ -1,2 +0,0 @@
-var myUrl = document.URL;
-alert(document.URL);
